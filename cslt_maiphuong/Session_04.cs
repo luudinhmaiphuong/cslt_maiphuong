a556b2ba83961c598e189dc36f6bdca5e50b3676
luudinhmaiphuong <luudinhmaiphuong2103@gmail.com>
2024-10-23 21:43:07 +0700

    them ham moi + sua ten

---
+++ cslt_maiphuong/Session_04.cs
@@ -0,0 +1,14 @@
+﻿internal class Session_04
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Session_04_ex_01()
+    {
+
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- cslt_maiphuong/Sression_04.cs
+++
@@ -1,7 +0,0 @@
-﻿internal class Sression_04
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
