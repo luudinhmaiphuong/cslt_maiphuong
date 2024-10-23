23d365adb13c6a1cf330ef4fcdf32544b8c9a770
luudinhmaiphuong <luudinhmaiphuong2103@gmail.com>
2024-10-23 23:29:30 +0700

    them bt 6

---
+++ cslt_maiphuong/Session06.cs
@@ -0,0 +1,33 @@
+﻿internal class Session_06
+{
+    public static void Main()
+    {
+        Question_01();
+    }
+    public static void Question_01()
+    {
+        Console.Write("nhap canh thu 1: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("nhap canh thu 2: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.Write("nhap canh thu 3: ");
+        int c = int.Parse(Console.ReadLine());
+        if ( a > 0 && b > 0 && c > 0 )
+        {
+            if (a == b && b == c)
+            {
+                Console.WriteLine("tam giac nay la tam giac can");
+            }
+            else if (a == b || b == c || c == a)
+            {
+                Console.WriteLine("tam giac nay la tam giac deu");
+            }
+            else
+            {
+                Console.WriteLine("tam giac nay la tam giac binh thuong");
+            }
+        else ()
+        }
+        
+    }
+}
---
+++ cslt_maiphuong/session04.cs
@@ -0,0 +1,13 @@
+﻿internal class Session_04
+{
+    public static void Main()
+    {
+        Question_01();
+    }
+    public static void Question_01()
+        //chay so tu 1-10, doan so dung theo may da chon
+    {
+        Random rnd = new Random();
+        int comp_num = rnd.Next(0, 10) + 1;
+        Console.Write(comp_num"Ban doan so may?")
+    }
