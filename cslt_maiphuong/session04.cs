6d4a38f3c8314e551b467725503fa09a11e6b9c8
Administrator <Administrator@N301PC21>
2024-10-30 23:14:16 +0700

    theem baif + suarw baifwerklewvnkle

--- cslt_maiphuong/session04.cs
+++ cslt_maiphuong/session04.cs
@@ -1,13 +1,48 @@
-﻿internal class Session_04
+﻿internal class Session_04_00
 {
-    public static void Main()
+    public static void Main4()
     {
-        Question_01();
+        Session04_00();
     }
-    public static void Question_01()
-        //chay so tu 1-10, doan so dung theo may da chon
+    public static void Session04_00()
     {
-        Random rnd = new Random();
-        int comp_num = rnd.Next(0, 10) + 1;
-        Console.Write(comp_num"Ban doan so may?")
+        do
+        {
+            //Máy nghĩ ngẫu nhiên 1 số
+            Random rnd = new Random();
+            int comp_num = rnd.Next(0, 10) + 1;
+            //Hỏi người dùng đoán số
+            //Đoán cho đến chừng nào đúng thì thôi
+            int count = 0;
+            bool isContinue = true;
+            do
+            {
+                count++;
+                Console.Write("Ban doan so may? <1...10>");
+                int user_num = int.Parse(Console.ReadLine());
+                //Kiểm tra kết quả
+                if (user_num == comp_num)
+                {
+                    Console.WriteLine("Ban la thien tai");
+                    isContinue = false;
+                }
+                else
+                {
+                    if (user_num > comp_num)
+                        Console.WriteLine("So ban doan lon hon may nghi");
+                    else
+                        Console.WriteLine("So ban doan nho hon may nghi");
+                }
+            } while (isContinue);
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Choi nua khong? <C/K>: ");
+            string tl = Console.ReadLine();
+            if (tl.ToUpper().Equals("K"))
+            {
+                Console.WriteLine("Thang ma khong cho go. Lan sau khong choi nua!");
+                return;
+            }
+        } while (true);
     }
+}
---
+++ cslt_maiphuong/session_07.cs
@@ -0,0 +1,50 @@
+﻿
+namespace cslt_maiphuong
+{
+    internal class session_07
+    {
+        //public static int MaxOfThreeNumbers(int a, int b, int c)
+        //{
+        //    //return Math.Max(a, Math.Max(b, c));
+        //    int max = 0;
+        //    if (a > b && a > c)
+        //        max = a;
+        //    else if ((b > c && b > a)) max = b;
+        //    else max = c;
+        //    return max;    
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int m = MaxOfThreeNumbers(5, 2, 4);
+        //    Comsole.Writeline(m);
+        //    Console.ReadKey();
+        //}
+        public static bool isPrime(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= number; i++)
+                if (number % i == 0) return false;
+            return true;
+        }
+        static void PrintPrimeNumberUnderN(int n)
+        {
+            for (int i=2;i<=n;i++)
+                if (isPrime(i))
+                    Console.WriteLine(i);
+        }
+        static void printFirstNprime (int n)
+        {
+            int so = 2;
+            int dem = 0;
+            while (dem < n)
+            {
+                if (isPrime(so))
+                {
+                    Console.Write(so + ",");
+                    dem++;
+                }
+                so++;
+            }
+        }
+    }
+}
